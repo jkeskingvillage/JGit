f1c371d5ce85767d814f370b1b90c0e6ea0783d9
Jungkoo Kang <jungkoo.kang@gmail.com>
2016-09-21 08:13:10 -0700

    Add the Initial JGit message

---
+++ JGit_Test/src/JGit_Test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : JGit_Test.cpp
+// Author      : Jungkoo Kang
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "JGit_Test" << std::endl;
+	return 0;
+}
